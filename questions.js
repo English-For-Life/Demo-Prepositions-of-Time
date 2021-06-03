a65abccbd44b3188332d327a0746d724c0f4dcff
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-06-04 00:02:05 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,32 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:" ....... Christmas Day",
+        options:["on", "at", "in"],
+        answer: 0
+    },
+    {
+        q:"I’m going out. I’ll be back ....... an hour.",
+        options:["in", "on", "at"],
+        answer: 0
+    },
+    {
+        q:" ....... half past two",
+        options:["on", "in", "at"],
+        answer: 2
+    },
+    {
+        q:"I’ll send you the money ....... the end of the month.",
+        options:["on", "in", "at"],
+        answer: 2
+    },
+    {
+        q:"Will you be at home ....... this evening?",
+        options:["in", "-", "at"],
+        answer: 1
+    }
+
+
+
+]
